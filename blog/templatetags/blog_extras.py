c407e45374181d6f940bef67df12c446641d4432
marcosBlasco <marcos.ielect@gmail.com>
2026-02-05 19:06:36 -0300

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django import template
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
+from blog.models import Post
 user_model = get_user_model()
 register = template.Library()
 
@@ -27,3 +28,27 @@ def author_details(author, current_user=None):
 
     return scaped_email
 
+
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+    return mark_safe("</div>")
+
+
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}">', extra_classes)
+
+
+@register.simple_tag
+def endcol():
+    return mark_safe("</div>")
+
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
